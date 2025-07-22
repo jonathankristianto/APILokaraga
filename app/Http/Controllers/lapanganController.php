d7f9debbb406900846004612761087744875beee
jonathankristianto <jonathankris10@gmail.com>
2025-07-23 05:56:38 +0700

    comm

--- app/Http/Controllers/lapanganController.php
+++ app/Http/Controllers/lapanganController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\lapangan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Resources\lapanganResource;
 use App\Http\Resources\lapanganDetailResource;
 
@@ -31,17 +32,26 @@ class lapanganController extends Controller
             'fasilitas' => 'required',
             'jam_buka_operasional' => 'required|date_format:H:i',
             'jam_tutup_operasional' => 'required|date_format:H:i',
-            'foto' => 'required|max:255',
             'user_id' => 'required|exists:user,id',
             'jenis_olahraga_id' => 'required|exists:jenis_olahraga,id',
         ]);
 
+        $foto = '';
+        if ($request->file) {
+            $fileName = $this->generateRandomString();
+            $extension = $request->file->extension();
+            $foto = $fileName.'.'.$extension;
+
+            Storage::putFileAs('foto', $request->file, $foto);
+        }
+
+        $request['foto'] = $foto;
         $request['user_id'] = Auth::user()->id;
         $lapangan = lapangan::create($request->all());
         return new lapanganResource ($lapangan->loadMissing('jenisolahraga','user'));
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id) 
     {
         $validated = $request->validate([
             'nm_lapangan' => 'required|max:100',
@@ -50,7 +60,6 @@ class lapanganController extends Controller
             'fasilitas' => 'required',
             'jam_buka_operasional' => 'required|date_format:H:i',
             'jam_tutup_operasional' => 'required|date_format:H:i',
-            'foto' => 'required|max:255',
             'user_id' => 'required|exists:user,id',
             'jenis_olahraga_id' => 'required|exists:jenis_olahraga,id',
         ]);
@@ -68,4 +77,16 @@ class lapanganController extends Controller
 
         return new lapanganResource ($lapangan->loadMissing('jenisolahraga','user'));
     }
+
+    function generateRandomString($length = 30) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+
+    return $randomString;
+}
 }
